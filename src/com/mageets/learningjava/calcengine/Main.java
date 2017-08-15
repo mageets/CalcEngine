884c1423e4812e6147a04ee34efacb6c880e2fa6
mageets <mageets@hotmail.com>
2017-08-15 23:03:51 +0100

    got back previous changes

--- src/com/mageets/learningjava/calcengine/Main.java
+++ src/com/mageets/learningjava/calcengine/Main.java
@@ -3,6 +3,15 @@ package com.mageets.learningjava.calcengine;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        MathEquation[] equations = new MathEquation[4];
+        equations[0] = new MathEquation('d',100,50);
+        equations[1] = new MathEquation('a',25,92);
+        equations[2] = new MathEquation('s',225,17);
+        equations[3] = new MathEquation('m',11,3);
+
+        for (MathEquation equation : equations) {
+            equation.execute();
+            System.out.println(equation.getResult());
+        }
     }
 }
